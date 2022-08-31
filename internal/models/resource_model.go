ec2ca014fe6f0bc0241d1d0bc38c473e7fe579a7
Anton <antonioo83@mail.ru>
2022-08-31 17:09:23 +0300

    Model, resource_model.go: "code" is string;

--- internal/models/resource_model.go
+++ internal/models/resource_model.go
@@ -5,10 +5,11 @@ import "time"
 type Resource struct {
 	ID          int       `copier:"-"`           // External resource ID.
 	UserId      int       `copier:"-"`           // User ID.
-	Code        int64     `copier:"-"`           // Unique identification of a resource in the external source.
+	Code        string    `copier:"-"`           // Unique identification of a resource in the external source.
 	URL         string    `copier:"URL"`         // URL of the resource where the request should be sent.
 	Description string    `copier:"Description"` // Additional comment for a resource.
 	CreatedAt   time.Time // Date and time the message was created.
 	UpdatedAt   time.Time // Date and time the message was updated.
 	DeletedAt   time.Time // Date and time the message was deleted.
+	Setting     Setting   // Setting relation.
 }
